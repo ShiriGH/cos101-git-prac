d8cfc38cfbec94663a89078cc45ba6d8455e1d79
Moegamat Anees Petersen <4255277@myuwc.ac.za>
2022-08-24 21:48:58 +0200

    GUI Math v1.1

--- GuiMath.java
+++ GuiMath.java
@@ -1,9 +1,17 @@
-//GUIMath.java
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
 
 class GUIMath implements ActionListener{
 
