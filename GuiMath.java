520850057637bd28d3deef5c1e95e530734fc766
Moegamat Anees Petersen <4255277@myuwc.ac.za>
2022-08-24 21:43:08 +0200

    GUI Math v1.0 with launch.json included

--- GuiMath.java
+++ GuiMath.java
@@ -1,50 +1,86 @@
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+//GUIMath.java
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+class GUIMath implements ActionListener{
 
-public class GuiMath implements ActionListener{
-
-    private int count = 0;
-    private JLabel label;
+    private int FirstNum;
+    private int SecondNum;
+    private int Sum;
     private JFrame frame;
-    private JButton button;
+    private JLabel labelCW;
+    private JLabel labelSum;
     private JPanel panel;
+    private JTextField textField;
+    private JButton button;
+    private int CountC;
+    private int CountW;
 
-    public GuiMath() {
+    public GUIMath(){
+        Random random = new Random();
+        FirstNum = random.nextInt(10);
+        SecondNum = random.nextInt(10);
+        Sum = FirstNum + SecondNum;
+        CountC = 0;
+        CountW = 0;
 
-        frame = new JFrame();
-        
-        button = new JButton("Click Me");
+        frame = new JFrame("Gui Math");
+
+        labelCW = new JLabel("Correct: " + str(CountC) + " Wrong: 0" + str(CountW));
+        labelCW.setHorizontalAlignment(SwingConstants.CENTER);
+        labelSum = new JLabel(str(FirstNum) + " + " + str(SecondNum) + " = ");
+        labelSum = setHorizontalAlignment(SwingConstants.CENTER);
+
+        textField = new JTextField(1);
+        textField.setHorizontalAlignment(SwingConstants.CENTER);
+
+        JButton button = new JButton("Submit");
         button.addActionListener(this);
-
-        label = new JLabel("Number of Click: 0");
-
+        
         panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
+        panel.setLayout(new GridLayout(0,1));
+        panel.add(labelCW);
+        panel.add(labelSum);
+        panel.add(textField);
         panel.add(button);
-        panel.add(label);
-
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("GUI Math");
+        
+        frame.add(panel, BorderLayout.NORTH);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
 
-    }
-    public static void main(String[] args) {
-        new GuiMath();
-    }
     @Override
     public void actionPerformed(ActionEvent e) {
-        count++;
-        label.setText("Number of Click: " + Integer.toString(count));
+        int input = Integer.parseInt(textField.getText());
+        if(input == Sum){
+            CountC++;
+            labelCW.setText("Correct: " + str(CountC) + " Wrong: " + str(CountW));
+            FirstNum = random.nextInt(10);
+            SecondNum = random.nextInt(10);
+            labelSum.setText(str(FirstNum) + " + " + str(SecondNum));
+        }
+        else if(input != Sum){
+            CountW++;
+            labelCW.setText("Correct: " + str(CountC) + " Wrong: ") + str(CountW);
+            FirstNum = random.nextInt(10);
+            SecondNum = random.nextInt(10);
+            labelSum.setText(str(FirstNum) + " + " + str(SecondNum))
+        }
+        else{
+            labelCW.setText("ERROR");
+            labelSum.setText("ERROR");
+        }
     }
+
+
+    public static void main(String args[]) {
+
+        GUIMath gui = new GUIMath();
+    }
+
 }
